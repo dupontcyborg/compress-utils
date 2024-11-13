009c19a016855bce213a6d01de30fb7dea6249a1
nicolas <ndupont@cyborg.co>
2024-11-13 16:04:37 -0500

    Debugging wheel structure

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -7,12 +7,8 @@ setup(
     author="Nicolas Dupont",
     license="MIT",
     packages=['compress_utils_py'],
-    package_dir={'compress_utils_py': 'bindings/python'},
     package_data={
         'compress_utils_py': [
-            'compress_utils_py*.so',    # For Linux shared libraries
-            'compress_utils_py*.dylib', # For macOS shared libraries
-            'compress_utils_py*.dll',   # For Windows shared libraries
             'README.md',
             'LICENSE'
         ]
